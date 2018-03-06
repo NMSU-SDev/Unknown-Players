30ffe5eec47596d0b599bd00f71d1cd095d07cbe
Andrew B <abandrewbaca@gmail.com>
2018-03-06 10:59:04 -0700

    add existing files

---
+++ New Unity Project/Assets/GM.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GM : MonoBehaviour {
+
+	public static int totalScore = 0;
+	public static float vertVel = 0;
+	public static float timeTotal = 0;
+	public static float zPos = 80;
+	public static float zPosColid = 60;
+
+	public static float zValAdj = 2.0f;
+
+	public Transform bb1;
+	public Transform bb2;
+	public Transform bb;
+
+	// Use this for initialization
+	void Start () {
+		Instantiate (bb, new Vector3 (0, 0, 40), bb.rotation);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ New Unity Project/Assets/moveOrb.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveOrb : MonoBehaviour {
+
+	public KeyCode moveLeft;
+	public KeyCode moveRight;
+
+	public float horizVel = 0;
+	public int laneNum = 2; 
+	public string controlLocked = "false";
+
+	public Transform boomObj;
+	public Transform path1;
+	public Transform colis;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Rigidbody> ().velocity = new Vector3(horizVel, GM.vertVel, 4*GM.zValAdj);
+
+		if ((Input.GetKeyDown(moveLeft)) &&(laneNum > 0) && (controlLocked == "false")) {
+			horizVel = -2;
+			laneNum -= 1;
+			StartCoroutine (stopSlide ());
+			controlLocked = "true";
+		}
+		if ((Input.GetKeyDown(moveRight))&&(laneNum < 6) && (controlLocked == "false")) {
+			horizVel = 2;
+			laneNum += 1;
+			StartCoroutine (stopSlide ());
+			controlLocked = "true";
+		}
+
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.tag == "killa") {
+			Destroy (gameObject);
+			GM.zValAdj = 0;
+			Instantiate (boomObj, transform.position, boomObj.rotation);
+		}
+
+		if (other.gameObject.name == "powerUp") {
+			Destroy (other.gameObject);
+		}
+		if (other.gameObject.tag == "spawner") {
+			Instantiate (path1, new Vector3(0, 0, GM.zPos), path1.rotation);
+			GM.zPos += 40;
+			Instantiate (colis, new Vector3 (0, 0, GM.zPosColid), colis.rotation);
+			GM.zPosColid += 40;
+			GM.zValAdj = 1;
+		}
+				
+	}
+
+	IEnumerator stopSlide()
+	{
+		yield return new WaitForSeconds(.5f);
+		horizVel = 0;
+		controlLocked = "false";
+	}
+}
